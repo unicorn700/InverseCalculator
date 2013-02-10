c2de189dd02a54567dd4e5f366928b98f7eb6433
bittugoyal700 <bittugoyal7000@gmail.com>
2013-02-10 09:57:51 +0530

    Initial commit

---
+++ src/inverse/Inverse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inverse;
+
+import UI.Home;
+
+/**
+ *
+ * @author unicorn
+ */
+public class Inverse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Home().setVisible(true);
+    }
+}
